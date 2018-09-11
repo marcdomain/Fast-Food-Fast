5415ca9b89bf4e114af0da1540b9fbb586a9eb4c
Marcus <marcus2cu@gmail.com>
2018-09-11 16:12:52 +0100

    [ch #160415021] Write more script to toggle Login/Signup

--- UI/js/toggle.js
+++ UI/js/toggle.js
@@ -1,34 +1,50 @@
-
 const authForm = () => {
   const selectDiv = document.querySelectorAll('.auth-form');
   const selectForm = document.querySelectorAll('form');
   const displayModal = document.querySelector('.modal');
-  
-  const toggleForm = (event) => {
-      if(event.target.id === 'login'){
-          selectDiv[0].style.display = 'none';
-          selectDiv[1].style.display = 'block';
-          displayModal.style.display = 'block';
 
-          setInterval(() => {
-            selectForm[1].removeAttribute('class');
-            selectForm[1].setAttribute('class', 'form-after-click');
-          }, 100);
-      }
-      if (event.target.id === 'signup') {
-          selectDiv[1].style.display = 'none';
-          selectDiv[0].style.display = 'block';
-          displayModal.style.display = 'block';
-          
-          setInterval(() => {
-            selectForm[0].removeAttribute('class');
-            selectForm[0].setAttribute('class', 'form-after-click');
-          }, 100);
-      }
+  const toggleForm = (event) => {
+    if (event.target.id === 'login') {
+      selectDiv[0].style.display = 'none';
+      selectDiv[1].style.display = 'block';
+      displayModal.style.display = 'block';
+
+      setInterval(() => {
+        selectForm[1].removeAttribute('class');
+        selectForm[1].setAttribute('class', 'form-after-click');
+      }, 100);
+    }
+    if (event.target.id === 'signup') {
+      selectDiv[1].style.display = 'none';
+      selectDiv[0].style.display = 'block';
+      displayModal.style.display = 'block';
+
+      setInterval(() => {
+        selectForm[0].removeAttribute('class');
+        selectForm[0].setAttribute('class', 'form-after-click');
+      }, 100);
+    }
+    if (event.target.id === 'login-modal') {
+      selectDiv[0].style.display = 'none';
+      selectDiv[1].style.display = 'block';
+      selectForm[1].removeAttribute('class');
+      selectForm[1].setAttribute('class', 'translate-form');
+    }
+    if (event.target.id === 'signup-modal') {
+      selectDiv[1].style.display = 'none';
+      selectDiv[0].style.display = 'block';
+      selectForm[0].removeAttribute('class');
+      selectForm[0].setAttribute('class', 'translate-form');
+    }
   }
   const signup = document.querySelector('#signup');
   const login = document.querySelector('#login');
+  const loginModal = document.querySelector('#login-modal');
+  const signupModal = document.querySelector('#signup-modal');
+
   signup.addEventListener('click', toggleForm);
   login.addEventListener('click', toggleForm);
+  loginModal.addEventListener('click', toggleForm);
+  signupModal.addEventListener('click', toggleForm);
 };
 window.load = authForm();
