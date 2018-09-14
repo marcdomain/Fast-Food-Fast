9ecf762536bf28547a696567e8a0778e5cefbae7
Marcus <marcus2cu@gmail.com>
2018-09-14 11:17:12 +0100

    [ch #160502050] test for homepage api endpoint

--- server/tests/orderTest.js
+++ server/tests/orderTest.js
@@ -5,3 +5,15 @@ import app from '../../app';
 const { expect } = chai;
 
 chai.use(chaiHttp);
+
+describe('Test Homepage API Endpoint', () => {
+  it('Should return status code 200 for success', (done) => {
+    chai.request(app)
+      .get('/api/v1')
+      .end((error, response) => {
+        expect(response).to.have.status(200);
+        expect(response.body.message).to.equal('Welcom to Fast-Food-Fast');
+        done();
+      });
+  });
+});
