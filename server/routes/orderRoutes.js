f8f93a6a2cf9c7df82cf9d717019dac3ce74bc97
Marcus <marcus2cu@gmail.com>
2018-09-14 15:42:39 +0100

    [ft #160503389] validate user input email

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import ordersController from '../controllers/ordersController';
+import validators from '../middlewares/orderInputValidations';
 
 const orderRouter = express.Router();
 
-orderRouter.post('/orders', ordersController.placeOrder);
+orderRouter.post('/orders', validators.placeOrderValidator, ordersController.placeOrder);
 
 export default orderRouter;
