3df394cb700eca8b3b63c5b88b9aaebdf9b38e47
Marcus <marcus2cu@gmail.com>
2018-09-14 15:06:29 +0100

    [ft #160503389] remove duplicate paths

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import router from './server/routes/defaultRoutes';
-import orderRouter from './server/routes/defaultRoutes';
+import { orderRoutes, defaultRoutes } from './server/routes/index';
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/api/v1', orderRouter);
-app.use('/', router);
+app.use('/api/v1', orderRoutes);
+app.use('/', defaultRoutes);
 
 const port = process.env.PORT || 5030;
 
