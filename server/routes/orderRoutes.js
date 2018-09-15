fd63f869bb414b528e781623757b77e8e2147e46
Marcus <marcus2cu@gmail.com>
2018-09-15 15:13:40 +0100

    [ft #160503632] create update-order-status controller and route

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -7,5 +7,6 @@ const orderRouter = express.Router();
 orderRouter.post('/orders', validators.placeOrderValidator, ordersController.placeOrder);
 orderRouter.get('/orders', ordersController.getAllOrders);
 orderRouter.get('/orders/:orderId', validators.fetchSpecificOrderValidator, ordersController.fetchSpecificOrder);
+orderRouter.put('/orders/:orderId', validators.fetchSpecificOrderValidator, ordersController.updateOrder);
 
 export default orderRouter;
