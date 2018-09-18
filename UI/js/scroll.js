8d44bb9837adc3b47f5e761e2090abab2adba0e0
Marcus <marcus2cu@gmail.com>
2018-09-18 17:18:48 +0100

    [ft #160541207] fix colour scheme and enhance user-experience

--- UI/js/scroll.js
+++ UI/js/scroll.js
@@ -1,47 +1,14 @@
 const scrollPage = () => {
-  const pizzaSection = document.querySelector('#pizza');
-  const localSection = document.querySelector('#local');
-  const riceSection = document.querySelector('#rice');
-  const snacksSection = document.querySelector('#snacks');
-  const othersSection = document.querySelector('#others');
-
-  const pizzaBtn = document.querySelector('.pizza');
-  const localBtn = document.querySelector('.local');
-  const riceBtn = document.querySelector('.rice');
-  const snacksBtn = document.querySelector('.snacks');
-  const othersBtn = document.querySelector('.others');
-
   const scrollTopBtn = document.querySelector('.scroll-top');
 
   const selectSection = (event) => {
-    if (event.target === pizzaBtn) {
-      pizzaSection.scrollIntoView();
-    }
-    if (event.target === localBtn) {
-      localSection.scrollIntoView();
-    }
-    if (event.target === riceBtn) {
-      riceSection.scrollIntoView();
-    }
-    if (event.target === snacksBtn) {
-      snacksSection.scrollIntoView();
-    }
-    if (event.target === othersBtn) {
-      othersSection.scrollIntoView();
-    }
     if (event.target === scrollTopBtn) {
       document.body.scrollTop = 0;
       document.documentElement.scrollTop = 0;
     }
-  }
-
-  pizzaBtn.addEventListener('click', selectSection);
-  localBtn.addEventListener('click', selectSection);
-  riceBtn.addEventListener('click', selectSection);
-  snacksBtn.addEventListener('click', selectSection);
-  othersBtn.addEventListener('click', selectSection);
+  };
 
   scrollTopBtn.addEventListener('click', selectSection);
-}
+};
 
 window.load = scrollPage();
