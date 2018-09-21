17274b1c3628481b6ee9c4b8618c32b49a13b9f1
Marcus <marcus2cu@gmail.com>
2018-09-21 10:44:08 +0100

    [ft #160633101] test for unaccepted signup input

--- server/tests/authTest.js
+++ server/tests/authTest.js
@@ -2,20 +2,196 @@ import chai from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../../app';
 
-import { correctUser, undefinedName } from './mockData/userMock';
+import {
+  correctUser, undefinedName, emptyName, invalidNameLength, invalidNameCharacter,
+  undefinedEmail, emptyEmail, invalidEmailLength, invalidEmailCharacter,
+  existingEmail, undefinedPhone, emptyPhone, invalidPhoneLength,
+  invalidPhoneCharacter, existingPhone, undefinedPassword, emptyPassword,
+  invalidPasswordLength
+} from './mockData/userMock';
 
 const { expect } = chai;
 
 chai.use(chaiHttp);
 
 describe('Test for Signup User', () => {
-  it.only('should return 201 for success', (done) => {
+  it('should return 201 for success', (done) => {
     chai.request(app)
       .post('/api/v1/auth/signup')
       .send(correctUser)
       .end((error, response) => {
         expect(response).to.have.status(201);
         expect(response.body.message).to.equal('Signed up successfully');
+        done();
+      });
+  });
+  it('should return 400 for undefined name', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(undefinedName)
+      .end((error, response) => {
+        expect(response).to.have.status(400);
+        expect(response.body.message).to.equal('Name is undefined');
+        done();
+      });
+  });
+  it('should return 400 for undefined name', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(emptyName)
+      .end((error, response) => {
+        expect(response).to.have.status(400);
+        expect(response.body.message).to.equal('Name cannot be empyt. Please input you name');
+        done();
+      });
+  });
+  it('should return 400 for undefined name', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(invalidNameLength)
+      .end((error, response) => {
+        expect(response).to.have.status(400);
+        expect(response.body.message).to.equal('Name should be 4 to 50 characters long');
+        done();
+      });
+  });
+  it('should return 400 for undefined name', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(invalidNameCharacter)
+      .end((error, response) => {
+        expect(response).to.have.status(400);
+        expect(response.body.message).to.equal('Name should be accepts alphabet and whitespace');
+        done();
+      });
+  });
+  it('should return 400 for undefined name', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(undefinedEmail)
+      .end((error, response) => {
+        expect(response).to.have.status(400);
+        expect(response.body.message).to.equal('Email is undefined');
+        done();
+      });
+  });
+  it('should return 400 for undefined name', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(emptyEmail)
+      .end((error, response) => {
+        expect(response).to.have.status(400);
+        expect(response.body.message).to.equal('Email field cannot be empty');
+        done();
+      });
+  });
+  it('should return 400 for undefined name', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(invalidEmailLength)
+      .end((error, response) => {
+        expect(response).to.have.status(400);
+        expect(response.body.message).to.equal('Email should be 10 to 50 characters');
+        done();
+      });
+  });
+  it('should return 400 for undefined name', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(invalidEmailCharacter)
+      .end((error, response) => {
+        expect(response).to.have.status(400);
+        expect(response.body.message).to.equal('Please input a valid email');
+        done();
+      });
+  });
+  it('should return 400 for undefined name', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(existingEmail)
+      .end((error, response) => {
+        expect(response).to.have.status(400);
+        expect(response.body.message).to.equal('Email already exist, please Signup with a new email');
+        done();
+      });
+  });
+  it('should return 400 for undefined name', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(undefinedPhone)
+      .end((error, response) => {
+        expect(response).to.have.status(400);
+        expect(response.body.message).to.equal('Phone is undefined');
+        done();
+      });
+  });
+  it('should return 400 for undefined name', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(emptyPhone)
+      .end((error, response) => {
+        expect(response).to.have.status(400);
+        expect(response.body.message).to.equal('Phone field cannot be empty');
+        done();
+      });
+  });
+  it('should return 400 for undefined name', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(invalidPhoneLength)
+      .end((error, response) => {
+        expect(response).to.have.status(400);
+        expect(response.body.message).to.equal('Phone should be 7 to 13 characters');
+        done();
+      });
+  });
+  it('should return 400 for undefined name', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(invalidPhoneCharacter)
+      .end((error, response) => {
+        expect(response).to.have.status(400);
+        expect(response.body.message).to.equal('Phone should be positive integer');
+        done();
+      });
+  });
+  it('should return 400 for undefined name', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(existingPhone)
+      .end((error, response) => {
+        expect(response).to.have.status(400);
+        expect(response.body.message).to.equal('Phone number already exist. Signup with a new one');
+        done();
+      });
+  });
+  it('should return 400 for undefined name', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(undefinedPassword)
+      .end((error, response) => {
+        expect(response).to.have.status(400);
+        expect(response.body.message).to.equal('Password is undefined');
+        done();
+      });
+  });
+  it('should return 400 for undefined name', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(emptyPassword)
+      .end((error, response) => {
+        expect(response).to.have.status(400);
+        expect(response.body.message).to.equal('Password field cannot be empty');
+        done();
+      });
+  });
+  it('should return 400 for undefined name', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send(invalidPasswordLength)
+      .end((error, response) => {
+        expect(response).to.have.status(400);
+        expect(response.body.message).to.equal('Password should be 4 to 20 characters');
         done();
       });
   });
--- server/tests/mockData/userMock.js
+++ server/tests/mockData/userMock.js
@@ -1,16 +1,128 @@
 const correctUser = {
   name: 'Marcus',
-  email: 'marcus@gmail.com',
+  email: 'marcus2cu@yahoo.com',
   phone: '07065266168',
   password: 'marcpass'
 };
 
 const undefinedName = {
   email: 'marcus@gmail.com',
+  phone: '07065266169',
+  password: 'marcpass'
+};
+
+const emptyName = {
+  name: '',
+  email: 'marcus@gmail.com',
+  phone: '07065266169',
+  password: 'marcpass'
+};
+
+const invalidNameLength = {
+  name: 'ma',
+  email: 'marcus@gmail.com',
+  phone: '07065266169',
+  password: 'marcpass'
+};
+
+const invalidNameCharacter = {
+  name: 'ma',
+  email: 'marcus@gmail.com',
+  phone: '07065266169',
+  password: 'marcpass'
+};
+
+const undefinedEmail = {
+  name: 'marc marc',
+  phone: '07065266169',
+  password: 'marcpass'
+};
+
+const emptyEmail = {
+  name: 'marc marc',
+  email: '',
+  phone: '07065266169',
+  password: 'marcpass'
+};
+
+const invalidEmailLength = {
+  name: 'marc marc',
+  email: 'ab@cd.ng',
+  phone: '07065266169',
+  password: 'marcpass'
+};
+
+const invalidEmailCharacter = {
+  name: 'marc marc',
+  email: 'ab##@c##d.ng',
+  phone: '07065266169',
+  password: 'marcpass'
+};
+
+const existingEmail = {
+  name: 'Marcus',
+  email: 'marcus2cu@yahoo.com',
+  phone: '07065266169',
+  password: 'marcpass'
+};
+
+const undefinedPhone = {
+  name: 'marc marc',
+  email: 'marc@gmail.ng',
+  password: 'marcpass'
+};
+
+const emptyPhone = {
+  name: 'marc marc',
+  email: 'marc@gmail.ng',
+  phone: '',
+  password: 'marcpass'
+};
+
+const invalidPhoneLength = {
+  name: 'marc marc',
+  email: 'marc@gmail.ng',
+  phone: '0706526',
+  password: 'marcpass'
+};
+
+const invalidPhoneCharacter = {
+  name: 'marc marc',
+  email: 'marc@gmail.ng',
+  phone: '07065266169#@',
+  password: 'marcpass'
+};
+
+const existingPhone = {
+  name: 'Marcus',
+  email: 'marcmarc@yahoo.com',
   phone: '07065266168',
   password: 'marcpass'
 };
 
+const undefinedPassword = {
+  name: 'marc marc',
+  email: 'marc@gmail.ng',
+  phone: '07065266169'
+};
+
+const emptyPassword = {
+  name: 'marc marc',
+  email: 'marc@gmail.ng',
+  phone: '07065266169',
+  password: ''
+};
+
+const invalidPasswordLength = {
+  name: 'marc marc',
+  email: 'marc@gmail.ng',
+  phone: '07065266169',
+  password: 'a'
+};
+
 export {
-  correctUser, undefinedName
+  correctUser, undefinedName, emptyName, invalidNameLength, invalidNameCharacter,
+  undefinedEmail, emptyEmail, invalidEmailLength, invalidEmailCharacter, existingEmail,
+  undefinedPhone, emptyPhone, invalidPhoneLength, invalidPhoneCharacter, existingPhone,
+  undefinedPassword, emptyPassword, invalidPasswordLength
 };
