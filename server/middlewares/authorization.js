658f40eda2b30c8242faa40fb513f53d4ad05df3
Marcus <marcus2cu@gmail.com>
2018-09-23 01:44:24 +0100

    [ft #160644256] verify-token

--- server/middlewares/authorization.js
+++ server/middlewares/authorization.js
@@ -6,4 +6,32 @@ const createToken = (payload) => {
   return token;
 };
 
-export default createToken;
+const verifyToken = (request, response, next) => {
+  const token = request.headers.authorization || request.body.token;
+  if (!token) {
+    return response.status(403)
+      .json({
+        status: 'Fail',
+        message: 'No token supplied',
+      });
+  }
+  jwt.verify(token, process.env.SECRETEKEY, (error, authData) => {
+    if (error) {
+      if (error.message.includes('signature')) {
+        return response.status(403)
+          .json({
+            status: 'Fail',
+            message: 'Your input is not a valid token. Please input a correct one',
+          });
+      }
+      return response.status(403)
+        .json({
+          message: error,
+        });
+    }
+    request.authData = authData;
+    return next();
+  });
+};
+
+export { createToken, verifyToken };
