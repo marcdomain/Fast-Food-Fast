51908de5ca4d2f6a6f0950fb9102242eb68de641
Marcus <marcus2cu@gmail.com>
2018-10-05 01:29:29 +0100

    [ft #160920703] style response data from get all menu enpoint

--- views/js/fetch/get-all-menu.js
+++ views/js/fetch/get-all-menu.js
@@ -8,38 +8,51 @@ const getAvailableMenu = (event) => {
   })
     .then(response => response.json())
     .then((data) => {
-      console.log('DATA', data.allMenu);
 
       let menuContainer = document.querySelector('.foodContainer');
 
-      data.allMenu.forEach((item, index, menuArray) => {
-        increament();
+      data.allMenu.forEach((item, index, itemArray) => {
         let newMenu = document.createElement('DIV');
         let menuName = document.createElement('DIV');
         menuName.innerHTML = `${item.menu}`;
         menuName.setAttribute('class', 'menuName');
         let image = document.createElement('IMG');
         image.setAttribute('class', 'menuImage');
-        image.setAttribute('src', `url('${item.imageurl}')`);
 
-        console.log('IMAGE URL', `${item.imageurl}`);
+        image.src = `${item.imageurl}`;
+
         let details = document.createElement('DIV');
         let description = document.createElement('DIV');
         let quantityInput = document.createElement('DIV');
         quantityInput.innerHTML = `
           <form id='order${item.id}'>
-            <input type='number' id='quantity${item.id}'>
-            <input type='submit' id='submit${item.id}'>
+            <input type='number' id='quantity${item.id}' placeholder='Qty' class='input'>
+            <input type='submit' id='submit${item.id}' value='send' class='input'>
           </form>
         `;
         description.innerHTML = `${item.description}`;
+        description.setAttribute('class', 'description');
         let orderAction = document.createElement('DIV');
+        orderAction.setAttribute('class', 'orderAction');
         let orderBtn = document.createElement('SPAN');
+        orderBtn.setAttribute('class', 'orderBtn');
         orderBtn.innerHTML = 'order now';
+
+        let price = document.createElement('DIV');
+        price.setAttribute('class', 'price');
+        price.innerHTML = `&#8358;${item.price}`;
+
+        quantityInput.setAttribute('class', 'quantityform');
+        const showQuantity = () => {
+          const quantityFields = document.querySelectorAll('.quantityform');
+          quantityFields[index].style.display = 'block';
+        };
+        orderBtn.addEventListener('click', showQuantity);
 
         orderAction.appendChild(orderBtn);
         orderAction.appendChild(quantityInput);
 
+        details.setAttribute('class', 'details');
         details.appendChild(description);
         details.appendChild(orderAction);
 
@@ -49,6 +62,7 @@ const getAvailableMenu = (event) => {
         newMenu.appendChild(menuName);
         newMenu.appendChild(image);
         newMenu.appendChild(details);
+        newMenu.appendChild(price);
         menuContainer.appendChild(newMenu);
       });
       document.querySelector('.foodContainer').innerHTML = [...menuContainer];
