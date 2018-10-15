2c479dcfb43269ff39701b87f7746c136b1e8361
Marcus <marcus2cu@gmail.com>
2018-10-15 11:50:50 +0100

    [ft 160921068] consume get specific-user order history endpoint

--- views/js/fetch/user-order-history.js
+++ views/js/fetch/user-order-history.js
@@ -9,7 +9,6 @@ const token = localStorage.getItem('token');
 const decoded = decodeUser(token);
 
 const userId = decoded.payload.payload.id;
-console.log('USER ID', userId);
 
 const userOrders = () => {
   fetch(`${baseURL}/users/${userId}/orders`, {
@@ -22,46 +21,43 @@ const userOrders = () => {
   })
     .then(data => data.json())
     .then((response) => {
-      console.log('RESPONSE', response);
-      // const allOrdersTable = document.querySelector('.all-orders-table');
+      const allOrdersTable = document.querySelector('.all-orders-table');
 
-      // response.allOrders.forEach((order, index, orderArray) => {
-      //   const eachOrderDiv = document.createElement('DIV');
-      //   eachOrderDiv.setAttribute('class', 'items');
-      //   const eachOrderTable = document.createElement('TABLE');
+      response.orderHistory.forEach((order, index, orderArray) => {
+        const eachOrderDiv = document.createElement('DIV');
+        eachOrderDiv.setAttribute('class', 'items');
+        const eachOrderTable = document.createElement('TABLE');
 
-      //   order.orderitems.forEach((item, count, itemArray) => {
-      //     const itemRow = document.createElement('TR');
-      //     itemRow.innerHTML = `
-      //       <td>
-      //         <strong>Meal:</strong> &nbsp; &nbsp; &nbsp; &nbsp; ${item.menu} <br>
-      //         <strong>Quantity:</strong> &nbsp;${item.quantity} <br>
-      //         <strong>Amount:</strong> &nbsp; &#8358; ${item.amount}
-      //       </td>
-      //     `;
-      //     eachOrderTable.appendChild(itemRow);
-      //   });
-      //   eachOrderDiv.appendChild(eachOrderTable);
+        order.orderitems.forEach((item, count, itemArray) => {
+          const itemRow = document.createElement('TR');
+          itemRow.innerHTML = `
+            <td>
+              <strong>Meal:</strong> &nbsp; &nbsp; &nbsp; &nbsp; ${item.menu} <br>
+              <strong>Quantity:</strong> &nbsp;${item.quantity} <br>
+              <strong>Amount:</strong> &nbsp; &#8358; ${item.amount}
+            </td>
+          `;
+          eachOrderTable.appendChild(itemRow);
+        });
+        eachOrderDiv.appendChild(eachOrderTable);
 
-      //   const newTableRow = document.createElement('TR');
-      //   newTableRow.innerHTML = `
-      //     <td>${order.id}</td>
-      //     <td>
-      //       ${new Date(order.orderdate).toString().split(' GMT')[0]}
-      //     </td>
-      //     <td>${order.phone}</td>
-      //     <td>${order.location}</td>
-      //     <td height="150" width="350">${eachOrderDiv.outerHTML}</td>
-      //     <td>&#8358;${order.total}</td>
-      //     <td class="status">
-      //       <span class="process">process</span> <br><br>
-      //       <span class="cancel">cancel</span>
-      //       <span class="complete">complete</span>
-      //     </td>
-      //   `;
-      //   allOrdersTable.appendChild(newTableRow);
-      // });
-      // return allOrdersTable;
+        const newTableRow = document.createElement('TR');
+        newTableRow.innerHTML = `
+          <td>${order.id}</td>
+          <td>
+            ${new Date(order.orderdate).toString().split(' GMT')[0]}
+          </td>
+          <td>${order.phone}</td>
+          <td>${order.location}</td>
+          <td height="150" width="350">${eachOrderDiv.outerHTML}</td>
+          <td>&#8358;${order.total}</td>
+          <td class="status">
+            ${order.status}
+          </td>
+        `;
+        allOrdersTable.appendChild(newTableRow);
+      });
+      return allOrdersTable;
     })
     .catch((error) => {
       console.log('Catch get all orders error', error);
