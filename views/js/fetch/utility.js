4dbb5233d29a7b339d3ea8bd29b8dabf31d66cdd
Marcus <marcus2cu@gmail.com>
2018-10-15 11:50:50 +0100

    [ft 160921068] Consume get specific-user order history endpoint

--- views/js/fetch/utility.js
+++ views/js/fetch/utility.js
@@ -1,4 +1,5 @@
 const baseURL = 'https://marcus-fast-food-fast.herokuapp.com/api/v1';
+// const baseURL = 'http://localhost:5030/api/v1';
 
 class Utils {
   static displayMessage(message, color, fieldIndex) {
