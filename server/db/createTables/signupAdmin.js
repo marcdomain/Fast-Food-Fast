027074873f592c58368123214f4e472ade2e2c73
Marcus <marcus2cu@gmail.com>
2018-11-02 21:34:36 +0100

    [ft #161302714] update page stylings

--- server/db/createTables/signupAdmin.js
+++ server/db/createTables/signupAdmin.js
@@ -1,9 +1,17 @@
 import bcrypt from 'bcrypt';
 import pool from '../connection';
+import 'dotenv/config';
 
 const sql = 'insert into users (name, email, phone, address, password, usertype) values ($1, $2, $3, $4, $5, $6)';
-const password = bcrypt.hashSync('adminuser', 10);
-const variables = ['Admin', 'admin@gmail.com', '08082300954', 'Andela EPIC Tower', password, 'admin'];
+const password = bcrypt.hashSync(process.env.ADMIN_PASSWORD, 10);
+const variables = [
+  process.env.ADMIN_NAME,
+  process.env.ADMIN_EMAIL,
+  process.env.ADMIN_PHONE,
+  process.env.ADMIN_ADDRESS,
+  password,
+  'admin'
+];
 
 class InsertAdminHandler {
   static createAdmin() {
